a0e64f3d2a20229070d9ef57c67ff9519e7304f6
vsvictor <dvictor74@gmail.com>
2017-05-15 21:59:58 +0300

    Fixed and worked QUIC, cronet 59, QUIC 35

---
+++ app/src/main/java/com/rev/quic/Const.java
@@ -0,0 +1,9 @@
+package com.rev.quic;
+
+/**
+ * Created by victor on 01.05.17.
+ */
+
+public class Const {
+    public static String sha256 = "2D:D0:23:52:0D:7C:3C:CE:A5:9B:A4:F3:D3:FB:3E:44:61:6A:A7:F7:EA:9F:05:41:6D:AA:2D:67:00:DC:13:1B";
+}
--- app/src/main/java/com/rev/quic/MainActivity.java
+++ app/src/main/java/com/rev/quic/MainActivity.java
@@ -1,22 +1,33 @@
 package com.rev.quic;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Button;
 
 import com.rev.quick.R;
 
+import org.chromium.base.PackageUtils;
+import org.chromium.net.ApiVersion;
 import org.chromium.net.CronetEngine;
 import org.chromium.net.CronetException;
 import org.chromium.net.UploadDataProviders;
 import org.chromium.net.UrlRequest;
 import org.chromium.net.UrlResponseInfo;
+import org.chromium.net.impl.JavaCronetProvider;
+import org.chromium.net.impl.NativeCronetProvider;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
@@ -24,7 +35,14 @@ import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.WritableByteChannel;
 import java.nio.charset.Charset;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
+import java.util.Set;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -32,9 +50,10 @@ public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private CronetEngine engine;
-
+    private Executor executor;
     private String mUrl;
     private WebView wvGoogle;
+    private File outputFile;
 
     class SimpleUrlRequestCallback extends UrlRequest.Callback {
         private ByteArrayOutputStream mBytesReceived = new ByteArrayOutputStream();
@@ -44,37 +63,35 @@ public class MainActivity extends Activity {
         public void onRedirectReceived(UrlRequest request, UrlResponseInfo info, String newLocationUrl) {
             Log.i(TAG, "****** onRedirectReceived ******");
             request.followRedirect();
+            Log.i(TAG, "Request: "+request.toString());
         }
 
         @Override
         public void onResponseStarted(UrlRequest request, UrlResponseInfo info) {
             Log.i(TAG, "****** Response Started ******");
-            Log.i(TAG, "*** Headers Are *** " + info.getAllHeaders());
+            Log.i("HEADERS", "*** Headers Are *** " + info.getAllHeaders());
+            Log.i(TAG, "UrlResponseInfo: "+info.toString());
             request.read(ByteBuffer.allocateDirect(32 * 1024));
         }
 
         @Override
-        public void onSucceeded(UrlRequest request, UrlResponseInfo info) {
-            Log.i(TAG, "****** Request Completed, status code is " + info.getHttpStatusCode() + ", total received bytes is " + info.getReceivedByteCount());
+        public void onSucceeded(final UrlRequest request, final UrlResponseInfo info) {
+            Log.i("RESPONSE", "****** Request Completed, status code is " + info.getHttpStatusCode() + ", total received bytes is " + info.getReceivedByteCount()+", protocol - "+info.getNegotiatedProtocol());
+
             final String receivedData = mBytesReceived.toString();
             final String url = info.getUrl();
             final String text = "Completed " + url + " (" + info.getHttpStatusCode() + ")";
             MainActivity.this.runOnUiThread(new Runnable() {
                 public void run() {
-                    Charset ch = Charset.forName("windows-1251");
-                    ByteBuffer res = ch.encode(receivedData);
-                    byte[] b = res.array();
-                    String str = new String(b);
-                    wvGoogle.loadData(str, "text/html; charset=windows-1251", "windowsd-1251");
-                    //wvGoogle.loadData(receivedData, "text/html; charset=UTF-8", "UTF-8");
-                    //wvGoogle.loadData(receivedData, "text/html; charset=ISO-8859-1", "ISO-8859-1");
-                    //wvGoogle.loadData(receivedData, "text/html", null);
+                    wvGoogle.loadData(receivedData, "text/html", null);
+
                 }
             });
         }
 
         @Override
         public void onReadCompleted(UrlRequest request, UrlResponseInfo info, ByteBuffer byteBuffer) {
+            Log.i("RESPONSE", "****** Request Completed, status code is " + info.getHttpStatusCode() + ", total received bytes is " + info.getReceivedByteCount()+", protocol - "+info.getNegotiatedProtocol());
             byteBuffer.flip();
             Log.i(TAG, "****** onReadCompleted ******" + byteBuffer);
             try {
@@ -104,28 +121,45 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         wvGoogle = (WebView) findViewById(R.id.wvGoogle);
         wvGoogle.getSettings().setJavaScriptEnabled (true);
-        //wvGoogle.getSettings().setDefaultTextEncodingName("utf-8");
-        //wvGoogle.getSettings().setDefaultTextEncodingName("windows-1251");
 
+        enableReadingPermissionForLogging();
+        enableWritingPermissionForLogging();
         CronetEngine.Builder builder = new CronetEngine.Builder(this);
-        builder.enableHttpCache(CronetEngine.Builder.HTTP_CACHE_IN_MEMORY, 100 * 1024)
-                .enableHttp2(true)
-                .enableQuic(true);
-
+        builder
+                .addQuicHint("monitor.revsw.net", 443, 443)
+                .enableQuic(true)
+                .enableHttp2(true);
         engine = builder.build();
-        String appUrl = "https://google.com";
-        startWithURL(appUrl);
-    }
+        Log.i("VERSION", "API version: "+ApiVersion.getCronetVersion());
+        NativeCronetProvider pr = new NativeCronetProvider(this);
+        Log.i("VERSION", "NativeCronetProvider"+pr.getVersion());
+        JavaCronetProvider jpr = new JavaCronetProvider(this);
+        Log.i("VERSION", "JavaCronetProvider"+jpr.getVersion());
 
-    private void applyPostDataToUrlRequestBuilder(UrlRequest.Builder builder, Executor executor, String postData) {
-        if (postData != null && postData.length() > 0) {
-            builder.setHttpMethod("POST");
-            builder.addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-            builder.setUploadDataProvider(
-                    UploadDataProviders.create(postData.getBytes()), executor);
+        try {
+            outputFile = File.createTempFile("nuubit", "log",
+                    Environment.getExternalStorageDirectory());
+            engine.startNetLogToFile(outputFile.toString(), false);
+            Log.i("PERMISSION",engine.getVersionString().toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        final String appUrl = "https://monitor.revsw.net:443/test-cache.js";
+        Button bSend = (Button) findViewById(R.id.bSend);
+        bSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startWithURL(appUrl);
+            }
+        });
+        ;
     }
-
+    @Override
+    public void onPause(){
+        super.onPause();
+        engine.stopNetLog();
+    }
     private void startWithURL(String url) {
         startWithURL(url, null);
     }
@@ -133,11 +167,53 @@ public class MainActivity extends Activity {
     private void startWithURL(String url, String postData) {
         Log.i(TAG, "Cronet started: " + url);
         mUrl = url;
-
-        Executor executor = Executors.newSingleThreadExecutor();
+        String userAgent = System.getProperty("http.agent");
+        Log.i("VERSION", userAgent);
+        executor = Executors.newSingleThreadExecutor();
         UrlRequest.Callback callback = new SimpleUrlRequestCallback();
-        UrlRequest.Builder builder = engine.newUrlRequestBuilder(url, callback, executor);
-        applyPostDataToUrlRequestBuilder(builder, executor, postData);
-        builder.build().start();
+        UrlRequest.Builder builder = engine.newUrlRequestBuilder(url, callback, executor)
+                .addHeader("Alternate-Protocol","443:quic")
+                .addHeader("User-Agent",userAgent);
+        UrlRequest req = builder.build();
+        req.start();
     }
+    private void enableWritingPermissionForLogging() {
+        int REQUEST_EXTERNAL_STORAGE = 1;
+        String[] PERMISSIONS_STORAGE = {
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+
+        int permission = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }else{
+            Log.i("PERMISSION", "Write granted");
+        }
+    }
+    private void enableReadingPermissionForLogging() {
+        int REQUEST_EXTERNAL_STORAGE = 1;
+        String[] PERMISSIONS_STORAGE = {
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        };
+
+        int permission = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }else{
+            Log.i("PERMISSION", "Readed granted");
+        }
+    }
+
 }
